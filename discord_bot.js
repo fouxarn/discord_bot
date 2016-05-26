7e72da3f213ea682a1e380294d82f045dc35c80d
Daniel Sjövall <dsjovall@gmail.com>
2016-05-26 14:06:47 +0200

    Full of brainpower

--- discord_bot.js
+++ discord_bot.js
@@ -127,6 +127,14 @@ var commands = {
             bot.leaveVoiceChannel(bot.voiceConnection.voiceChannel);
         }
     },
+
+    "power": {
+        description: "BRAINPOWER!",
+        channel: getBotChannelName(),
+        process: function(bot, message, args) {
+            bot.sendMessage(message.channel, "O-oooooooooo AAAAE-A-A-I-A-U- JO-oooooooooooo AAE-O-A-A-U-U-A- E-eee-ee-eee AAAAE-A-E-I-E-A-JO-ooo-oo-oo-oo EEEEO-A-AAA-AAAA");
+        }
+    },
 };
 
 bot.on("ready", function() {
