4ff79b4c8912933368b7214f586ccff3dcd2df35
Daniel Sjövall <dsjovall@gmail.com>
2016-05-30 19:45:59 +0200

    Beautify

--- discord_bot.js
+++ discord_bot.js
@@ -3,7 +3,9 @@ var settings = require('./settings.json');
 var ytdl = require('ytdl-core');
 var storage = require('./lib/storage.js');
 
-var bot = new Discord.Client({autoReconnect: true});
+var bot = new Discord.Client({
+    autoReconnect: true
+});
 var store = new storage();
 
 let queue = [];
@@ -40,19 +42,19 @@ var commands = {
         description: "Add audio from a youtube link to the queue",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          if (args.length === 0) {
-            bot.sendMessage(message.channel, "You have to bring a link with the command");
-          } else {
-            playing = true;
-            if (args[0] === "random") {
-              store.getRandomUrl((url) => {
-                addToQueue(url, message);
-              });
+            if (args.length === 0) {
+                bot.sendMessage(message.channel, "You have to bring a link with the command");
             } else {
-              addToQueue(args[0], message);
-              store.storeTrackUrl(args[0]);
+                playing = true;
+                if (args[0] === "random") {
+                    store.getRandomUrl((url) => {
+                        addToQueue(url, message);
+                    });
+                } else {
+                    addToQueue(args[0], message);
+                    store.storeTrackUrl(args[0]);
+                }
             }
-          }
         }
     },
 
@@ -60,7 +62,7 @@ var commands = {
         description: "List all the songs in queue",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          listQueue(message.channel);
+            listQueue(message.channel);
         }
     },
 
@@ -68,7 +70,7 @@ var commands = {
         description: "Clears the queue and stops the music",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          clearQueue(message);
+            clearQueue(message);
         }
     },
 
@@ -76,7 +78,7 @@ var commands = {
         description: "Stops playing",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          stopPlaying(message);
+            stopPlaying(message);
         }
     },
 
@@ -84,7 +86,7 @@ var commands = {
         description: "Starts with next song in the queue",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          playing = true;
+            playing = true;
         }
     },
 
@@ -92,7 +94,7 @@ var commands = {
         description: "Skip to the next song in queue",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          playNextTrack();
+            playNextTrack();
         }
     },
 
@@ -101,30 +103,30 @@ var commands = {
         description: "Selects in which channel the bot should live",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-          bot.joinVoiceChannel(args[0], function(error) {
-      		    console.log(error.message);
-      	  });
+            bot.joinVoiceChannel(args[0], function(error) {
+                console.log(error.message);
+            });
         }
     },
 
     "cat": {
-      description: "I will give you my favorite cat image <3",
-      process: function(bot, message, args) {
-        bot.sendFile(message, "http://placekitten.com/" + (Math.floor((Math.random() * 900) + 100)+"/"+Math.floor((Math.random() * 900) + 100)), "cat.png", (err, msg) => {
-          if(err)
-            console.log("couldn't send image:", err);
-          });
-      }
+        description: "I will give you my favorite cat image <3",
+        process: function(bot, message, args) {
+            bot.sendFile(message, "http://placekitten.com/" + (Math.floor((Math.random() * 900) + 100) + "/" + Math.floor((Math.random() * 900) + 100)), "cat.png", (err, msg) => {
+                if (err)
+                    console.log("couldn't send image:", err);
+            });
+        }
     },
 
     "joinme": {
         description: "Join the channel of the user",
         channel: getBotChannelName(),
         process: function(bot, message, args) {
-            if(message.author.voiceChannel) {
+            if (message.author.voiceChannel) {
                 bot.joinVoiceChannel(message.author.voiceChannel, function(error) {
-            		    console.log(error.message);
-            	  });
+                    console.log(error.message);
+                });
             }
         }
     },
@@ -145,12 +147,12 @@ var commands = {
     },
 
     "toplist": {
-      description: "Prints usage toplist",
-      process: function(bot, message, args) {
-        store.getCommandLog((string) => {
-          bot.sendMessage(message.channel, string);
-        });
-      }
+        description: "Prints usage toplist",
+        process: function(bot, message, args) {
+            store.getCommandLog((string) => {
+                bot.sendMessage(message.channel, string);
+            });
+        }
     },
 };
 
@@ -158,29 +160,29 @@ bot.on("ready", function() {
     console.log("Ready! Serving on " + bot.servers.length + " servers :)");
     bot.setPlayingGame("with your feelings");
     bot.servers.forEach(function(server) {
-      if(server.channels.get("name", getBotChannelName(bot)) == null) {
-        console.log("The channel '" + getBotChannelName(bot) + "' doesn't exist on server " + server.name);
-      }
+        if (server.channels.get("name", getBotChannelName(bot)) == null) {
+            console.log("The channel '" + getBotChannelName(bot) + "' doesn't exist on server " + server.name);
+        }
     });
     checkQueue();
 });
 
-bot.on("message", function(message){
+bot.on("message", function(message) {
     if (message.author.id != bot.user.id && message.content[0] === settings.botPrefix) {
         console.log("Treating " + message.content + " from " + message.author.username + " as a command.");
         let command = message.content.substring(1).split(" ");
         let cmd = commands[command[0]];
 
         if (command[0] === "help") {
-          printCommands(message.channel);
-        } else if(cmd) {
-            try{
-                if (cmd.channel == undefined || cmd.channel == message.channel.name ) {
-                  command.shift();
-                  cmd.process(bot, message, command);
-                  store.logCommand(message.author);
+            printCommands(message.channel);
+        } else if (cmd) {
+            try {
+                if (cmd.channel == undefined || cmd.channel == message.channel.name) {
+                    command.shift();
+                    cmd.process(bot, message, command);
+                    store.logCommand(message.author);
                 }
-            } catch(e) {
+            } catch (e) {
                 bot.sendMessage(message.channel, "And it was at that moment Daniel knew, he fucked up");
                 console.log(e.stack);
             }
@@ -189,25 +191,25 @@ bot.on("message", function(message){
 });
 
 function printCommands(channel) {
-  let msg = "Available Commands: \n ```";
-  for (let cmd in commands) {
-    msg += settings.botPrefix + cmd;
-    let usage = commands[cmd].usage;
-    if (usage) {
-      msg += ` ${usage}`;
+    let msg = "Available Commands: \n ```";
+    for (let cmd in commands) {
+        msg += settings.botPrefix + cmd;
+        let usage = commands[cmd].usage;
+        if (usage) {
+            msg += ` ${usage}`;
+        }
+        let restricted = commands[cmd].channel;
+        if (restricted) {
+            msg += ` (in #${restricted})`;
+        }
+        let description = commands[cmd].description;
+        if (description) {
+            msg += `\n\t${description}`;
+        }
+        msg += "\n\n";
     }
-    let restricted = commands[cmd].channel;
-    if (restricted) {
-      msg += ` (in #${restricted})`;
-    }
-    let description = commands[cmd].description;
-    if (description) {
-        msg += `\n\t${description}`;
-    }
-    msg += "\n\n";
-  }
-  msg += "```";
-  bot.sendMessage(channel, msg);
+    msg += "```";
+    bot.sendMessage(channel, msg);
 }
 
 function getBotChannelName(client) {
@@ -215,83 +217,85 @@ function getBotChannelName(client) {
 }
 
 function checkQueue() {
-	if(playing && !queueEmpty() && !bot.voiceConnection.playing) {
-		playNextTrack();
-	}
+    if (playing && !queueEmpty() && !bot.voiceConnection.playing) {
+        playNextTrack();
+    }
 
-	setTimeout(checkQueue, 5000);
+    setTimeout(checkQueue, 5000);
 }
 
 function addToQueue(videoID, message) {
-  ytdl.getInfo(videoID, (error, info) => {
-    if (error) {
-      console.log(error);
-      bot.reply(message, "There was a problem adding the video.");
-    } else {
-      let track = {
-        title: info.title,
-        author: info.author,
-        view_count: info.view_count,
-        length_seconds: info.length_seconds,
-        info: info
-      };
-      queue.push(track);
-      bot.reply(message, "Added song to queue! :)");
-    }
-  });
+    ytdl.getInfo(videoID, (error, info) => {
+        if (error) {
+            console.log(error);
+            bot.reply(message, "There was a problem adding the video.");
+        } else {
+            let track = {
+                title: info.title,
+                author: info.author,
+                view_count: info.view_count,
+                length_seconds: info.length_seconds,
+                info: info
+            };
+            queue.push(track);
+            bot.reply(message, "Added song to queue! :)");
+        }
+    });
 }
 
 function listQueue(channel) {
-  let message = "Songqueue: \n ```";
-  for (let index in queue) {
-    let track = queue[index];
-    message += `${index}. ${track.title} by ${track.author}, [${track.view_count}] views, ${track.length_seconds} seconds\n\n`;
-  };
-  message += "```";
-  bot.sendMessage(channel, message);
+    let message = "Songqueue: \n ```";
+    for (let index in queue) {
+        let track = queue[index];
+        message += `${index}. ${track.title} by ${track.author}, [${track.view_count}] views, ${track.length_seconds} seconds\n\n`;
+    };
+    message += "```";
+    bot.sendMessage(channel, message);
 }
 
 function playNextTrack() {
-	if(queueEmpty()) {
-		bot.voiceConnection.stopPlaying();
-		return;
-	}
+    if (queueEmpty()) {
+        bot.voiceConnection.stopPlaying();
+        return;
+    }
 
-  try{
-    var options = {
-      filter: (format) => format.container === 'mp4',
-      quality: 'lowest',
-    };
-    let stream = ytdl.downloadFromInfo(queue[0].info, options);
-    stream.on('error', function(error) {
-      console.log(error);
-    });
+    try {
+        var options = {
+            filter: (format) => format.container === 'mp4',
+            quality: 'lowest',
+        };
+        let stream = ytdl.downloadFromInfo(queue[0].info, options);
+        stream.on('error', function(error) {
+            console.log(error);
+        });
 
-    bot.voiceConnection.playRawStream(stream, {volume: 0.2});
-  } catch (exception) {
-    console.log(exception);
-  }
+        bot.voiceConnection.playRawStream(stream, {
+            volume: 0.2
+        });
+    } catch (exception) {
+        console.log(exception);
+    }
 
-	queue.splice(0,1);
+    queue.splice(0, 1);
 }
 
 function stopPlaying(message) {
-  bot.voiceConnection.stopPlaying();
-  playing = false;
-  bot.sendMessage(message, "Stopped playing audio");
+    bot.voiceConnection.stopPlaying();
+    playing = false;
+    bot.sendMessage(message, "Stopped playing audio");
 }
 
 function clearQueue(message) {
-  queue = [];
-  bot.reply(message, "The queue has been cleared!");
+    queue = [];
+    bot.reply(message, "The queue has been cleared!");
 }
 
 function queueEmpty() {
-  return queue.length === 0;
+    return queue.length === 0;
 }
 
 bot.loginWithToken(settings.token).catch((error) => {
-  console.log(error);
+    console.log(error);
 })
 
 // Handle discord.js warnings
@@ -300,22 +304,22 @@ bot.on('debug', (m) => console.log('[debug]', m));
 
 // Taget från https://github.com/meew0/Lethe/blob/master/lethe.js#L571
 process.on('uncaughtException', function(err) {
-  // Handle ECONNRESETs caused by `next` or `destroy`
-  if (err.code == 'ECONNRESET') {
-    // Yes, I'm aware this is really bad node code. However, the uncaught exception
-    // that causes this error is buried deep inside either discord.js, ytdl or node
-    // itself and after countless hours of trying to debug this issue I have simply
-    // given up. The fact that this error only happens *sometimes* while attempting
-    // to skip to the next video (at other times, I used to get an EPIPE, which was
-    // clearly an error in discord.js and was now fixed) tells me that this problem
-    // can actually be safely prevented using uncaughtException. Should this bother
-    // you, you can always try to debug the error yourself and make a PR.
-    console.log('Got an ECONNRESET! This is *probably* not an error. Stacktrace:');
-    console.log(err.stack);
-  } else {
-    // Normal error handling
-    console.log(err);
-    console.log(err.stack);
-    process.exit(0);
-  }
+    // Handle ECONNRESETs caused by `next` or `destroy`
+    if (err.code == 'ECONNRESET') {
+        // Yes, I'm aware this is really bad node code. However, the uncaught exception
+        // that causes this error is buried deep inside either discord.js, ytdl or node
+        // itself and after countless hours of trying to debug this issue I have simply
+        // given up. The fact that this error only happens *sometimes* while attempting
+        // to skip to the next video (at other times, I used to get an EPIPE, which was
+        // clearly an error in discord.js and was now fixed) tells me that this problem
+        // can actually be safely prevented using uncaughtException. Should this bother
+        // you, you can always try to debug the error yourself and make a PR.
+        console.log('Got an ECONNRESET! This is *probably* not an error. Stacktrace:');
+        console.log(err.stack);
+    } else {
+        // Normal error handling
+        console.log(err);
+        console.log(err.stack);
+        process.exit(0);
+    }
 });
